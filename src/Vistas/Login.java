8ccd2a9cf556f6411db5f53ab93c067dc2e4b10a
Jorge <Jorge@Jorge-PC>
2016-08-26 17:32:55 -0400

    probando

--- src/Vistas/Login.java
+++ src/Vistas/Login.java
@@ -107,9 +107,7 @@ public class Login extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
        String user=txtRut.getText().trim();
        String password=new String(pssClave.getPassword());
-       if(user==null){
-           
-       }
+       
         // TODO add our handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
