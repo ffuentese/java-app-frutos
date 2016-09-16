f01b3bb04152db9859bba63f49c0d7bdcca8d7c0
Francisco Fuentes <txi.francisco@gmail.com>
2016-09-15 23:54:25 -0300

    Arreglo de problemas

---
+++ build/generated-sources/jax-ws/localhost/webservice/AutenticarResponse.java
@@ -0,0 +1,160 @@
+
+package localhost.webservice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import org.w3._2001.xmlschema.Schema;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="AutenticarResult" minOccurs="0">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;element ref="{http://www.w3.org/2001/XMLSchema}schema"/>
+ *                   &lt;any/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "autenticarResult"
+})
+@XmlRootElement(name = "AutenticarResponse")
+public class AutenticarResponse {
+
+    @XmlElement(name = "AutenticarResult")
+    protected AutenticarResponse.AutenticarResult autenticarResult;
+
+    /**
+     * Obtiene el valor de la propiedad autenticarResult.
+     * 
+     * @return
+     *     possible object is
+     *     {@link AutenticarResponse.AutenticarResult }
+     *     
+     */
+    public AutenticarResponse.AutenticarResult getAutenticarResult() {
+        return autenticarResult;
+    }
+
+    /**
+     * Define el valor de la propiedad autenticarResult.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link AutenticarResponse.AutenticarResult }
+     *     
+     */
+    public void setAutenticarResult(AutenticarResponse.AutenticarResult value) {
+        this.autenticarResult = value;
+    }
+
+
+    /**
+     * <p>Clase Java para anonymous complex type.
+     * 
+     * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;element ref="{http://www.w3.org/2001/XMLSchema}schema"/>
+     *         &lt;any/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "schema",
+        "any"
+    })
+    public static class AutenticarResult {
+
+        @XmlElement(namespace = "http://www.w3.org/2001/XMLSchema", required = true)
+        protected Schema schema;
+        @XmlAnyElement(lax = true)
+        protected Object any;
+
+        /**
+         * Obtiene el valor de la propiedad schema.
+         * 
+         * @return
+         *     possible object is
+         *     {@link Schema }
+         *     
+         */
+        public Schema getSchema() {
+            return schema;
+        }
+
+        /**
+         * Define el valor de la propiedad schema.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link Schema }
+         *     
+         */
+        public void setSchema(Schema value) {
+            this.schema = value;
+        }
+
+        /**
+         * Obtiene el valor de la propiedad any.
+         * 
+         * @return
+         *     possible object is
+         *     {@link Object }
+         *     
+         */
+        public Object getAny() {
+            return any;
+        }
+
+        /**
+         * Define el valor de la propiedad any.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link Object }
+         *     
+         */
+        public void setAny(Object value) {
+            this.any = value;
+        }
+
+    }
+
+}
---
+++ build/generated/jax-wsCache/Service1/localhost/webservice/ObjectFactory.java
@@ -0,0 +1,120 @@
+
+package localhost.webservice;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the localhost.webservice package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: localhost.webservice
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link ComunasResponse }
+     * 
+     */
+    public ComunasResponse createComunasResponse() {
+        return new ComunasResponse();
+    }
+
+    /**
+     * Create an instance of {@link AutenticarResponse }
+     * 
+     */
+    public AutenticarResponse createAutenticarResponse() {
+        return new AutenticarResponse();
+    }
+
+    /**
+     * Create an instance of {@link RegionesResponse }
+     * 
+     */
+    public RegionesResponse createRegionesResponse() {
+        return new RegionesResponse();
+    }
+
+    /**
+     * Create an instance of {@link ComunasResponse.ComunasResult }
+     * 
+     */
+    public ComunasResponse.ComunasResult createComunasResponseComunasResult() {
+        return new ComunasResponse.ComunasResult();
+    }
+
+    /**
+     * Create an instance of {@link AutenticarResponse.AutenticarResult }
+     * 
+     */
+    public AutenticarResponse.AutenticarResult createAutenticarResponseAutenticarResult() {
+        return new AutenticarResponse.AutenticarResult();
+    }
+
+    /**
+     * Create an instance of {@link Autenticar }
+     * 
+     */
+    public Autenticar createAutenticar() {
+        return new Autenticar();
+    }
+
+    /**
+     * Create an instance of {@link RegistrarClienteResponse }
+     * 
+     */
+    public RegistrarClienteResponse createRegistrarClienteResponse() {
+        return new RegistrarClienteResponse();
+    }
+
+    /**
+     * Create an instance of {@link Comunas }
+     * 
+     */
+    public Comunas createComunas() {
+        return new Comunas();
+    }
+
+    /**
+     * Create an instance of {@link Regiones }
+     * 
+     */
+    public Regiones createRegiones() {
+        return new Regiones();
+    }
+
+    /**
+     * Create an instance of {@link RegistrarCliente }
+     * 
+     */
+    public RegistrarCliente createRegistrarCliente() {
+        return new RegistrarCliente();
+    }
+
+    /**
+     * Create an instance of {@link RegionesResponse.RegionesResult }
+     * 
+     */
+    public RegionesResponse.RegionesResult createRegionesResponseRegionesResult() {
+        return new RegionesResponse.RegionesResult();
+    }
+
+}
